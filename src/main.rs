7abc03d2f9b6285d0d6de73c0d58dbaa0bad399c
Guy Marshall <guymarshall9@gmail.com>
2026-05-16 16:19:00 +0100

    WIP: Match against extension for future code paths

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ mod image;
 mod star;
 
 use std::error::Error;
+use std::ffi::OsStr;
 use std::fs;
+use std::path::Path;
 
 use crate::dimensions::Dimensions;
 
@@ -18,15 +20,37 @@ fn main() -> Result<(), Box<dyn Error>> {
     let buffer: Vec<u8> = fs::read("test_file_path.txt")?;
     let test_file_path: String = String::from_utf8(buffer)?.trim().to_string();
 
-    // get image dimensions
-    // TODO: add FitReader for .fit files
-    // TODO: add TifReader for .tif files
-    // TODO: add TifsReader for .tifs files
-    // TODO: add PngReader for .png files
-    // TODO: merge all readers into one reader
-    let dimensions: Dimensions = fits_reader::get_image_dimensions(&test_file_path)?;
+    let extension: &str = Path::new(&test_file_path)
+        .extension()
+        .and_then(OsStr::to_str)
+        .unwrap();
 
-    println!("{:?}", dimensions);
+    // TODO: get all image data into structure from this match, instead of just image dimensions
+    // TODO: merge all readers into one reader
+    match extension {
+        "fits" => {
+            // get image dimensions
+            let dimensions: Dimensions = fits_reader::get_image_dimensions(&test_file_path)?;
+            println!("{:?}", dimensions);
+        }
+        "fit" => {
+            // TODO: add FitReader for .fit files
+            todo!()
+        }
+        "tif" => {
+            // TODO: add TifReader for .tif files
+            todo!()
+        }
+        "tifs" => {
+            // TODO: add TifsReader for .tifs files
+            todo!()
+        }
+        "png" => {
+            // TODO: add PngReader for .png files
+            todo!()
+        }
+        _ => panic!("Unrecognised file extension."),
+    }
 
     // get image data
 
